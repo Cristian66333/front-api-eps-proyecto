73e39ab8795252010fbea8ecc8d2c6045bea99c9
Cristian66333 <74688238+Cristian66333@users.noreply.github.com>
2023-08-02 19:21:42 -0500

    readme

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const path = require('path')
 const cors = require('cors')
 
 const app = express()
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
